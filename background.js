767f1570c3a1be0cf984859c956c7396406d40f6
Derek Kim <ddkim1024@gmail.com>
2020-10-07 11:03:59 -0500

    Added interval display to user on popup, made requested changes on issue #5

--- background.js
+++ background.js
@@ -2,31 +2,35 @@ chrome.browserAction.setBadgeText({text: 'OFF'});
 chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
 let reloads_holder = {};
 
-chrome.storage.onChanged.addListener((whatchanged, area) => {
-    const currentKey = Object.keys(whatchanged)[0];
-    const currentVal = whatchanged[currentKey].newValue;
-    if(area === "local" && typeof currentVal === 'boolean' ) {
-        chrome.storage.local.get('input', (result) => {
-            if (currentVal) {
-                // Start reloading current tab
-                const reloadInterval = result.input ? result.input * 1000 : 10000;
+// Main auto-reload function
+chrome.storage.onChanged.addListener((whatChanged, area) => {
+    if(area === "local") {
+        const currentKey = Object.keys(whatChanged)[0];
+        const currentVal = whatChanged[currentKey].newValue;
 
-                reloads_holder[currentKey] = setInterval(()=>{
+        if (typeof currentVal.active === 'boolean') {
+            if (currentVal.active) {
+                // Start reloading current tab
+                const reloadInterval = currentVal.input ? currentVal.input * 1000 : 10000;
+
+                reloads_holder[currentKey] = setInterval(() => {
                     chrome.tabs.reload(parseInt(currentKey));
                 }, reloadInterval);
             } else {
                 // Stop reloading
                 clearInterval(reloads_holder[currentKey]);
             }
-        });
+        }
     }
 });
 
+// Update badge text
 chrome.tabs.onActivated.addListener(()=>{
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         let tab = tabs[0];
+
         chrome.storage.local.get(tab.id.toString(), (items) => {
-            if (items[tab.id]) {
+            if (items[tab.id] && items[tab.id].active) {
                 chrome.browserAction.setBadgeText({ text: 'ON' });
             } else {
                 chrome.browserAction.setBadgeText({ text: 'OFF' });
--- popup.js
+++ popup.js
@@ -1,46 +1,52 @@
-let active = document.getElementById('active');
+const active = document.getElementById('active');
 const reloadInput = document.getElementById('reload-int-input');
 
 // Maintain checked input box if this window has active reload
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   let tab = tabs[0];
   chrome.storage.local.get(tab.id.toString(), (items) => {
-      if(items[tab.id]){
-          console.log("found!");
-          active.checked = true;
-      }
+    // Load reload interval if previously inputted. Otherwise, default value is 10 seconds
+    if (items[tab.id]) {
+      reloadInput.value = items[tab.id].input ? items[tab.id].input : 10;
+    }
+
+    // Check off checkbox if currently reloading
+    if(items[tab.id] && items[tab.id].active){
+        active.checked = true;
+    }
   });
 });
 
 // Save current window tab to Chrome storage
 active.onchange = function(event) {
+  // Save reload interval parameter to Chrome storage
+  const numInput = parseInt(reloadInput.value);
+
   if (event.target.checked) {
-    
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
       let tab = tabs[0];
       let storedObj = {};
-      storedObj[tab.id] = true;
+      storedObj[tab.id] = {
+        active: true,
+        input: numInput
+      };
       chrome.storage.local.set(storedObj);
     });
 
     chrome.browserAction.setBadgeText({text: 'ON'});
 
   } else {
-
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
       let tab = tabs[0];
       let storedObj = {};
-      storedObj[tab.id] = false;
+      storedObj[tab.id] = {
+        active: false,
+        input: numInput
+      };
       chrome.storage.local.set(storedObj);
     });
 
     chrome.browserAction.setBadgeText({text: 'OFF'});
 
   }
-}
-
-// Save reload interval parameter to Chrome storage
-reloadInput.onchange = function(event) {
-  const numInput = parseInt(reloadInput.value);
-  chrome.storage.local.set({ input: numInput });
 }
