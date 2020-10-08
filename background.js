f4f2008166dd3234b0c1f4b61d1c45a5e85d5f80
ayushi_keshri <ayushi.keshri@nearex.com>
2020-10-08 21:32:31 +0530

    fixed: chrome extension tab id error

--- background.js
+++ background.js
@@ -1,36 +1,42 @@
-chrome.browserAction.setBadgeText({text: 'OFF'});
-chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+chrome.browserAction.setBadgeText({ text: "OFF" });
+chrome.browserAction.setBadgeBackgroundColor({ color: "#4688F1" });
 let reloads_holder = {};
+let count = 0;
+function callback(element) {
+  if (chrome.runtime.lastError) {
+    console.log(chrome.runtime.lastError.message);
+    clearInterval(reloads_holder[element]);
+  }
+}
 
 chrome.storage.onChanged.addListener((whatchanged, area) => {
-    if(area==="local"){
-        let keys = Object.keys(whatchanged);
-        keys.forEach(element => {
-            console.log(whatchanged[element]);
-            if(whatchanged[element].newValue){
-                reloads_holder[element] = setInterval(()=>{
-                    chrome.tabs.reload(parseInt(element));
-                }, 10000);
-            }
-            else {
-                clearInterval(reloads_holder[element]);
-            }
-        });
+  console.log(area);
+  if (area === "local") {
+    let keys = Object.keys(whatchanged);
+    keys.forEach((element) => {
+      console.log(element);
+      console.log(whatchanged[element].newValue);
 
-        
-    }
+      if (whatchanged[element].newValue) {
+        reloads_holder[element] = setInterval(() => {
+          chrome.tabs.reload(parseInt(element), () => callback(element));
+        }, 10000);
+      } else {
+        clearInterval(reloads_holder[element]);
+      }
+    });
+  }
 });
 
-chrome.tabs.onActivated.addListener(()=>{
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        let tab = tabs[0];
-        chrome.storage.local.get(tab.id.toString(), (items) => {
-            if(items[tab.id]){
-                chrome.browserAction.setBadgeText({text: 'ON'});
-            }
-            else {
-                chrome.browserAction.setBadgeText({text: 'OFF'});
-            }
-        });
-      });
+chrome.tabs.onActivated.addListener(() => {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    let tab = tabs[0];
+    chrome.storage.local.get(tab.id.toString(), (items) => {
+      if (items[tab.id]) {
+        chrome.browserAction.setBadgeText({ text: "ON" });
+      } else {
+        chrome.browserAction.setBadgeText({ text: "OFF" });
+      }
+    });
+  });
 });
